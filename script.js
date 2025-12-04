574451f080b94518c1d9af2a37c0317a8cd94521
BasaniKavya <kavyareddy9440@gmail.com>
2025-12-04 14:03:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,4 +11,25 @@ window.onclick = function(e) {
   const popup = document.getElementById("resumePopup");
   if (e.target === popup) popup.style.display = "none";
 }
+// ===== Scroll Reveal Animation =====
+function reveal() {
+  let reveals = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let revealTop = reveals[i].getBoundingClientRect().top;
+    let revealPoint = 120;
+
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+reveal();
+
+
 
