49fbbeb0854c8a05440b6af67ab692f9cd4f8e72
BasaniKavya <kavyareddy9440@gmail.com>
2025-12-04 11:03:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function openResumePopup() {
+  document.getElementById("resumePopup").style.display = "flex";
+}
+
+function closeResumePopup() {
+  document.getElementById("resumePopup").style.display = "none";
+}
+
+// Close popup if user clicks outside box
+window.onclick = function(e) {
+  const popup = document.getElementById("resumePopup");
+  if (e.target === popup) popup.style.display = "none";
+}
