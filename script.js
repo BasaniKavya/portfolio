370bd5da5447a50a0c2b8d539750c242a09103af
BasaniKavya <kavyareddy9440@gmail.com>
2025-12-06 16:04:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -207,4 +207,24 @@ async function loadRepos() {
   }
 }
 loadRepos();
+const username = "BasaniKavya";
+
+fetch(`https://api.github.com/users/${username}/repos`)
+  .then(res => res.json())
+  .then(data => {
+    const container = document.getElementById("projects-container");
+    data.slice(0, 6).forEach(repo => {   // fetch only 6 latest repos
+      const card = document.createElement("div");
+      card.classList.add("project-card");
+      card.innerHTML = `
+        <h3>${repo.name}</h3>
+        <p>${repo.description ? repo.description : "No description added."}</p>
+        <a href="${repo.html_url}" target="_blank">View on GitHub →</a>
+      `;
+      container.appendChild(card);
+    });
+  })
+  .catch(err => console.error("Error fetching repos:", err));
+
+
 
